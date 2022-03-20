a484c8d5df1a852d321468fea358cdac879f1b44
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-20 15:54:48 +0530

    feat: add style guide

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
 import "./App.css";
 
 const App = () => {
-	return <div className="App">This is app</div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
