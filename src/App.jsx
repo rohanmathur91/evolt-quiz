46f6b42e4e19f904f710b2edb88eaa4b85f24ff7
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-21 00:49:21 +0530

    run prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { Navbar } from "./components";
 import "./App.css";
 
 const App = () => {
-	return (
-		<div className="App">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
