96268ee474d305aaee12cab0cc9148603be61dff
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-20 17:05:04 +0530

    fix(style): navbar style fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar flex-row content-space-between items-center fixed top-0 z-1 w-100  py-2 px-6">
+		<nav className="navbar flex-row content-space-between items-center sticky top-0 z-1 w-100 py-2 px-6">
 			<Link to="/" className="text-xl font-black">
 				Evolt
 			</Link>
