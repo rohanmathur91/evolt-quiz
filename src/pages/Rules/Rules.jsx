2fce2bc51070650ea8a3729589f52f3bdf91d4f6
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-21 15:38:30 +0530

    fix(style): rules pages

--- src/pages/Rules/Rules.jsx
+++ src/pages/Rules/Rules.jsx
@@ -6,16 +6,16 @@ export const Rules = () => {
     <div className="flex-column items-center mt-4">
       <h3>Rules</h3>
       <div className={`${styles.content} mt-4 mx-2 p-4 rounded-sm`}>
-        <p className="mb-3 text-lg">
-          1. For each <span className="correct text-lg">correct</span> answer
+        <p className="mb-3 text-title">
+          1. For each <span className="correct text-title">correct</span> answer
           gets +2 points.
         </p>
-        <p className="mb-3 text-lg">
-          2. For each <span className="wrong text-lg">wrong</span> answer gets
-          -2 points.
+        <p className="mb-3 text-title">
+          2. For each <span className="wrong text-title">wrong</span> answer
+          gets -2 points.
         </p>
-        <p className="mb-3 text-lg">3. Only one chance for each answer.</p>
-        <p className="mb-3 text-lg">All the Best.</p>
+        <p className="mb-3 text-title">3. Only one chance for each answer.</p>
+        <p className="text-title">All The Best.</p>
       </div>
       <a
         className="mt-4 mb-2 cta py-1 px-2 font-semibold primary-cta rounded-sm"
