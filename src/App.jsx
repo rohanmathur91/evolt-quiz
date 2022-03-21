189843b7150195189a2565e24a5c6812c1d1727c
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-21 15:30:07 +0530

    fix: number of rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, Rules } from "./pages";
 import { Navbar } from "./components";
 import "./App.css";
 
 const App = () => {
-	return (
-		<div className="App">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Rules/Rules.jsx
+++ src/pages/Rules/Rules.jsx
@@ -3,28 +3,25 @@ import styles from "./Rules.module.css";
 
 export const Rules = () => {
   return (
-    <div className="flex-column items-center mt-8">
-      <a className="mt-6 text-xl font-bold" href="./rules.html">
-        Rules
-      </a>
+    <div className="flex-column items-center mt-4">
+      <h3>Rules</h3>
       <div className={`${styles.content} mt-4 mx-2 p-4 rounded-sm`}>
-        <p className="mb-3 text-lg">1. Quiz is free and open to all persons.</p>
         <p className="mb-3 text-lg">
-          2. For each <span className="correct text-lg">correct</span> answer
-          gets +2 points 😎
+          1. For each <span className="correct text-lg">correct</span> answer
+          gets +2 points.
         </p>
         <p className="mb-3 text-lg">
-          3. For each <span className="wrong text-lg">wrong</span> answer gets
-          -2 points 😯
+          2. For each <span className="wrong text-lg">wrong</span> answer gets
+          -2 points.
         </p>
-        <p className="mb-3 text-lg">4. Only one chance for each answer.</p>
-        <p className="mb-3 text-lg">5. Quiz is free and open to all persons.</p>
+        <p className="mb-3 text-lg">3. Only one chance for each answer.</p>
+        <p className="mb-3 text-lg">All the Best.</p>
       </div>
       <a
         className="mt-4 mb-2 cta py-1 px-2 font-semibold primary-cta rounded-sm"
         href="../quiz/quiz.html"
       >
-        Let's start.
+        Let's start
       </a>
     </div>
   );
