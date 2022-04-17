f7c66ff55fdee279ff9e1e7837ea6dc792b053bb
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-17 17:47:52 +0530

    fix(style): responsiveness for large screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useTheme } from "../../contexts";
 import styles from "./Navbar.module.css";
 
@@ -7,28 +7,46 @@ export const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <nav
-      className={`${styles.navbar} flex-row content-space-between items-center sticky top-0 z-1 w-100 py-2 px-6`}
-    >
-      <Link to="/" className={`${styles.heading} font-semibold`}>
-        Evolt Quiz
-      </Link>
-      <div className="flex-row items-center">
-        <Link
-          to="/login"
-          className={`${styles.loginCta} transition-2 rounded-sm text-base font-regular`}
-        >
-          Login
+    <nav className={`${styles.navbar} sticky top-0 z-1 w-100 py-2 px-5`}>
+      <div className="max-container flex-row content-space-between items-center">
+        <Link to="/" className={`${styles.heading} font-semibold`}>
+          Evolt Quiz
         </Link>
-        <button
-          title="Change theme"
-          onClick={toggleTheme}
-          className={`${styles.themeBtn} rounded-sm icon-container ml-1 transition-2`}
-        >
-          <span className="material-icons-outlined">
-            {theme === "dark" ? "light_mode" : "dark_mode"}
-          </span>
-        </button>
+
+        <div className="flex-row items-center">
+          <NavLink
+            to="/login"
+            title="Login"
+            className={({ isActive }) =>
+              `${styles.navIcon} ${
+                isActive ? styles.active : ""
+              } transition-2 rounded-sm icon-container font-regular`
+            }
+          >
+            <span className="material-icons-round">person_outline</span>
+          </NavLink>
+          <NavLink
+            to="/leaderboard"
+            title="Leaderboard"
+            className={({ isActive }) =>
+              `${styles.navIcon} ${
+                isActive ? styles.active : ""
+              } transition-2 rounded-sm icon-container font-regular`
+            }
+          >
+            <span className="material-icons-round">leaderboard</span>
+          </NavLink>
+
+          <button
+            title="Change theme"
+            onClick={toggleTheme}
+            className={`${styles.navIcon} rounded-sm icon-container transition-2`}
+          >
+            <span className="material-icons-outlined">
+              {theme === "dark" ? "light_mode" : "dark_mode"}
+            </span>
+          </button>
+        </div>
       </div>
     </nav>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { Home } from "./Home/Home";
-export { Category } from "./Category/Category";
 export { Quiz } from "./Quiz/Quiz";
 export { Result } from "./Result/Result";
+export { Category } from "./Category/Category";
+export { Leaderboard } from "./Leaderboard/Leaderboard";
