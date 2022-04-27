8c4f8a2883ce58acb2bc01d9e2e4c7b055ef56d3
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-28 01:18:01 +0530

    fix: cleanup

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -10,31 +10,19 @@ export const CategoryCard = ({
   category,
   numberOfquestions,
 }) => {
-  const handleShareQuiz = () => {
-    navigator.clipboard.writeText(`http://localhost:3000/quiz/${_id}`);
-  };
-
   return (
     <div className={`${styles.card} rounded-sm m-2 p-1`}>
       <img className={`${styles.image} rounded-sm`} src={image} alt={alt} />
       <h4 className="mt-2">{category}</h4>
       <p className="my-1">{title}</p>
-      <p className="text-sm text-gray mb-2">{numberOfquestions} Questions</p>
-      <div className="category-footer flex-row items-center">
+      <div className="category-footer flex-row items-center mt-3">
+        <p className="text-sm text-gray">{numberOfquestions} Questions</p>
         <Link
           to={`/quiz/${_id}`}
-          className="cta primary-cta rounded-sm transition-2"
+          className="cta primary-cta ml-auto rounded-sm transition-2"
         >
           Take quiz
         </Link>
-
-        <button
-          data-tooltip="Share quiz"
-          onClick={handleShareQuiz}
-          className="tooltip icon-container ml-auto outlined-btn rounded-sm"
-        >
-          <span className="material-icons-outlined sub-title">share</span>
-        </button>
       </div>
     </div>
   );
